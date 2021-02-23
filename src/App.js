51f4371cb6c258df9e254bd140ede291925a9319
Kunal Manchanda <kunal.manchanda9999@gmail.com>
2021-02-23 16:47:10 +0530

    init

--- src/App.js
+++ src/App.js
@@ -10,3 +10,9 @@ const App = () => {
 }
 
 export default App
+
+// git add . add all files to the staging area to be commited to local repository
+// git rm --cached <file> remove file from staging area to untracked file
+// git restore --staged <file> not staged for commit
+// git restore <file> to discard changes in working directory
+
